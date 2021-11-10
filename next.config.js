2248472f7204149bf60d06161f8c2f416097ace7
Oscar Busk <oscar.busk@gmail.com>
2021-11-10 01:30:21 +0100

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/(npm-?diff)(.?app)?",
+                destination: "https://npm-diff.app",
+                permanent: false,
+                basePath: false,
+            },
+            {
+                source: "/(mg|management-?game)",
+                destination: "https://mg.obusk.se",
+                permanent: false,
+                basePath: false,
+            },
+            {
+                source: "/(rectangle)(-?that)?",
+                destination: "https://rectangle.obusk.se",
+                permanent: false,
+                basePath: false,
+            },
+            {
+                source: "/(ng|nonogram)",
+                destination: "https://ng.obusk.se",
+                permanent: false,
+                basePath: false,
+            },
+        ];
+    },
 };
