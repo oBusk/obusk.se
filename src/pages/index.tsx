7f0ffa5e53a3c10ac5df8b1c4f02dc4ca5f6183a
Oscar Busk <oscar.busk@gmail.com>
2022-09-28 01:35:51 +0200

    Fix 100vh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { FunctionComponent } from "react";
+import Div100vh from "react-div-100vh";
 import { Composition } from "^/components/Composition";
 
 type Props = {};
@@ -14,34 +15,38 @@ const IndexPage: FunctionComponent<Props> = ({}) => {
                     content="This is the personal website of Oscar Busk"
                 />
             </Head>
-            <main>
-                <header>
-                    <a
-                        href="https://github.com/oBusk"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        github.com/oBusk
-                    </a>
-                    <h1>
-                        <a href="https://obusk.se">obusk.se</a>
-                    </h1>
-                    <a
-                        href="https://npm-diff.app"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        npm-diff.app
-                    </a>
-                </header>
-                {/* <Composition /> */}
-                <footer>
-                    <span>
-                        Oscar Busk - {new Date().getFullYear()} -{" "}
-                        <a href="mailto:contact@obusk.se">contact@obusk.se</a>
-                    </span>
-                </footer>
-            </main>
+            <Div100vh>
+                <main>
+                    <header>
+                        <a
+                            href="https://github.com/oBusk"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            github.com/oBusk
+                        </a>
+                        <h1>
+                            <a href="https://obusk.se">obusk.se</a>
+                        </h1>
+                        <a
+                            href="https://npm-diff.app"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            npm-diff.app
+                        </a>
+                    </header>
+                    {/* <Composition /> */}
+                    <footer>
+                        <span>
+                            Oscar Busk - {new Date().getFullYear()} -{" "}
+                            <a href="mailto:contact@obusk.se">
+                                contact@obusk.se
+                            </a>
+                        </span>
+                    </footer>
+                </main>
+            </Div100vh>
         </>
     );
 };
