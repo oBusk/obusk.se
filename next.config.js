410b05f535e91e5029c98e1fac00779506c9c92f
Oscar Busk <oscar.busk@gmail.com>
2025-12-03 13:42:05 +0100

    Remove some outdated projects

--- next.config.js
+++ next.config.js
@@ -11,26 +11,8 @@ const nextConfig = {
                 basePath: false,
             },
             {
-                source: "/(npm-?scan)(ner)?",
-                destination: "https://npm-scanner.vercel.app",
-                permanent: false,
-                basePath: false,
-            },
-            {
-                source: "/(mg|management-?game)",
-                destination: "https://mg.obusk.se",
-                permanent: false,
-                basePath: false,
-            },
-            {
                 source: "/(rectangle)(-?that)?",
                 destination: "https://rectangle.obusk.se",
-                permanent: false,
-                basePath: false,
-            },
-            {
-                source: "/(ng|nonogram)",
-                destination: "https://ng.obusk.se",
                 permanent: false,
                 basePath: false,
             },
