c6c6ebafdee7bf71bd06f6aed15358750daaa066
Oscar Busk <oscar.busk@gmail.com>
2026-08-04 12:08:45 +0200

    Update to next 16.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,22 @@
 import nextObusk from "@obusk/eslint-config-next";
 
-const eslintConfig = [...nextObusk];
+const eslintConfig = [
+    ...nextObusk,
+    {
+        settings: {
+            react: { version: "19" },
+        },
+        rules: {
+            "tailwindcss/classnames-order": "off",
+            "tailwindcss/enforces-negative-arbitrary-values": "off",
+            "tailwindcss/enforces-shorthand": "off",
+            "tailwindcss/important-modifier-suffix": "off",
+            "tailwindcss/no-arbitrary-value": "off",
+            "tailwindcss/no-contradicting-classname": "off",
+            "tailwindcss/no-custom-classname": "off",
+            "tailwindcss/no-unnecessary-arbitrary-value": "off",
+        },
+    },
+];
 
 export default eslintConfig;
